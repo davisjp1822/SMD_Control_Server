cfd7cd75da8920979e36a64da03cb326bc2bac6e
John Davis <jd@linecardapp.com>
2015-10-25 16:01:07 -0400

    implementing connecting threading and mutexing like a boss....

--- SMD_Control_Server/src/SMD_Modbus.c
+++ SMD_Control_Server/src/SMD_Modbus.c
@@ -10,13 +10,139 @@
 #include "SMD_Constants.h"
 #include "SMD_Utilities.h"
 #include "SMD_SocketOps.h"
+#include "SMD_Motor_Commands.h"
 
 #include <modbus.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
+
+typedef struct send_modbus_command_args {
+	
+	const int *registers;
+	const int *values;
+	int num_registers;
+	const char *command_name;
+	SMD_RESPONSE_CODES result;
+	
+} send_modbus_command_args;
+
+typedef struct read_modbus_command_args {
+	
+	const uint16_t *registers;
+	SMD_REGISTER_READ_TYPE reg_read_type;
+	int cl;
+	SMD_RESPONSE_CODES result;
+	
+} read_modbus_command_args;
+
+int LOCK_COMMS;
+pthread_mutex_t lock;
+
+/* Local function declarations */
+
+static void *_send_modbus_command(void *args);
+static void *_read_modbus_registers(void *args);
+
+/* Public function bodies */
 
 SMD_RESPONSE_CODES send_modbus_command(const int *registers, const int *values, const int num_registers, const char *command_name) {
+	
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		printf("Cannot get mutex\n");
+		return 0;
+	}
+	
+	else if(LOCK_COMMS == 1) {
+		
+		printf("Comms are locked when trying to send modbus command\n");
+	}
+	
+	else {
+		
+		pthread_mutex_lock(&lock);
+		
+		LOCK_COMMS = 1;
+		
+		pthread_t tid;
+		send_modbus_command_args cmd_args;
+		
+		cmd_args.registers = registers;
+		cmd_args.values = values;
+		cmd_args.num_registers = num_registers;
+		cmd_args.command_name = command_name;
+		
+		if(pthread_create(&tid, NULL, _send_modbus_command, &cmd_args) != 0) {
+			
+			log_message("Could not create Modbus connection thread!\n");
+		}
+		
+		else {
+			
+			LOCK_COMMS = 0;
+			pthread_mutex_unlock(&lock);
+			
+			pthread_join(tid, NULL);
+			return cmd_args.result;
+		}
+	}
+	
+	log_message("FATAL: Modbus threading should never have made it here!!!\n");
+	return 0;
+}
+
+SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_REGISTER_READ_TYPE reg_read_type, const int cl) {
+	
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		printf("Cannot get mutex\n");
+		return 0;
+	}
+	
+	else if(LOCK_COMMS == 1) {
+		
+		printf("Comms are locked when trying to send modbus command\n");
+	}
+	
+	else {
+		
+		pthread_mutex_lock(&lock);
+		
+		LOCK_COMMS = 1;
+		
+		pthread_t tid;
+		read_modbus_command_args cmd_args;
+		
+		cmd_args.registers = registers;
+		cmd_args.reg_read_type = reg_read_type;
+		cmd_args.cl = cl;
+		
+		if(pthread_create(&tid, NULL, _read_modbus_registers, &cmd_args) != 0) {
+			
+			log_message("Could not create Modbus read registers thread!\n");
+		}
+		
+		else {
+			
+			LOCK_COMMS = 0;
+			
+			pthread_mutex_unlock(&lock);
+			
+			pthread_join(tid, NULL);
+			return cmd_args.result;
+		}
+		
+	}
+	
+	log_message("FATAL: Modbus read registers threading should never have made it here!!!\n");
+	return 0;
+}
+
+/* Local function bodies */
+
+static void *_send_modbus_command(void *args) {
+	
+	send_modbus_command_args *cmd_args = (send_modbus_command_args *)args;
 	
 	modbus_t *ctx = NULL;
 	ctx = modbus_new_tcp(DEVICE_IP, 502);
@@ -25,15 +151,12 @@ SMD_RESPONSE_CODES send_modbus_command(const int *registers, const int *values, 
 	if( strlen(DEVICE_IP) == 0 || modbus_connect(ctx) == -1 ) {
 		
 		char message[1024];
-		char formattedIP[strlen(DEVICE_IP)-1];
-		
-		strncpy(formattedIP, DEVICE_IP, sizeof(formattedIP));
 		
 		snprintf(message,
 				 sizeof(message),
 				 "Connection failed when trying to connect to motor (%s) to send command %s with error: %s\n",
-				 formattedIP,
-				 command_name,
+				 DEVICE_IP,
+				 cmd_args->command_name,
 				 modbus_strerror(errno));
 		
 		log_message(message);
@@ -41,51 +164,78 @@ SMD_RESPONSE_CODES send_modbus_command(const int *registers, const int *values, 
 		modbus_close(ctx);
 		modbus_free(ctx);
 		
+		if(errno == 9) {
+			
+			log_message("Sleeping due to connection overflow\n");
+			nanosleep((const struct timespec[]){{0, 50000000L}}, NULL);
+			
+			//if the previous command was a hold move or immediate stop, resend it so that we can stop!!!!!!
+			if(strncmp(cmd_args->command_name, "hold_move", strlen(cmd_args->command_name)) == 0) {
+				
+				log_message("!!! Caught a failed hold_move command!\n");
+				SMD_hold_move();
+			}
+			
+			if(strncmp(cmd_args->command_name, "immediate_stop", strlen(cmd_args->command_name)) == 0) {
+				
+				log_message("!!! Caught a failed immediate_stop command!\n");
+				SMD_immed_stop();
+			}
+		}
+		
 		//TODO - this should fail more gracefully - just tell it connection numbers exceeded?
-		return SMD_RETURN_COMMAND_FAILED;
+		//return SMD_RETURN_COMMAND_FAILED;
+		cmd_args->result = SMD_RETURN_COMMAND_FAILED;
 	}
 	
 	else {
 		
 		int rc, i;
 		
-		for(i=0; i<num_registers; i++) {
-			rc = modbus_write_register(ctx, registers[i], values[i]);
-				
+		for(i=0; i<cmd_args->num_registers; i++) {
+			rc = modbus_write_register(ctx, cmd_args->registers[i], cmd_args->values[i]);
+			
 			if( rc == -1 ) {
 				
 				char message[1024];
 				snprintf(message,
 						 sizeof(message),
 						 "Error in calling modbus_write_register for command %s with error: %s\n",
-						 command_name,
+						 cmd_args->command_name,
 						 modbus_strerror(errno));
 				
 				log_message(message);
-
-				return SMD_RETURN_COMMAND_FAILED;
+				
+				//return SMD_RETURN_COMMAND_FAILED;
+				cmd_args->result = SMD_RETURN_COMMAND_FAILED;
 			}
 			
 			char message[1024];
 			snprintf(message,
 					 sizeof(message),
 					 "Successfully wrote value %d to Modbus register %d for command %s\n",
-					 values[i],
-					 registers[i],
-					 command_name);
+					 cmd_args->values[i],
+					 cmd_args->registers[i],
+					 cmd_args->command_name);
 			
 			log_message(message);
-
+			
 		}
 		
 		modbus_close(ctx);
 		modbus_free(ctx);
 		
-		return SMD_RETURN_COMMAND_SUCCESS;
+		//return SMD_RETURN_COMMAND_SUCCESS;
+		cmd_args->result = SMD_RETURN_COMMAND_SUCCESS;
 	}
+	
+	pthread_exit(NULL);
+	return 0;
 }
 
-SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_REGISTER_READ_TYPE reg_read_type, const int cl) {
+static void *_read_modbus_registers(void *args) {
+	
+	read_modbus_command_args *cmd_args = (read_modbus_command_args *)args;
 	
 	modbus_t *ctx = NULL;
 	ctx = modbus_new_tcp(DEVICE_IP, 502);
@@ -100,8 +250,8 @@ SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_RE
 		snprintf(errorString, sizeof(errorString), "Could not connect to SMD when trying to read input registers with error: %s\n", modbus_strerror(errno));
 		log_message(errorString);
 		
-		return SMD_RETURN_NO_ROUTE_TO_HOST;
-		
+		//return SMD_RETURN_NO_ROUTE_TO_HOST;
+		cmd_args->result = SMD_RETURN_NO_ROUTE_TO_HOST;
 	}
 	
 	else {
@@ -119,8 +269,8 @@ SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_RE
 			snprintf(errorString, sizeof(errorString), "Could not read input registers with error: %s\n", modbus_strerror(errno));
 			log_message(errorString);
 			
-			return SMD_RETURN_COMMAND_FAILED;
-			
+			//return SMD_RETURN_COMMAND_FAILED;
+			cmd_args->result = SMD_RETURN_COMMAND_FAILED;
 		}
 		
 		else {
@@ -142,11 +292,11 @@ SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_RE
 					
 					char idString[4];
 					
-					if(reg_read_type == SMD_READ_INPUT_REGISTERS) {
+					if(cmd_args->reg_read_type == SMD_READ_INPUT_REGISTERS) {
 						strncpy(idString, ",,\0", strlen(",,\0"));
 					}
 					
-					if(reg_read_type == SMD_READ_CONFIG_REGISTERS) {
+					if(cmd_args->reg_read_type == SMD_READ_CONFIG_REGISTERS) {
 						strncpy(idString, "###\0", strlen("###\0"));
 					}
 					
@@ -162,13 +312,16 @@ SMD_RESPONSE_CODES read_modbus_registers(const uint16_t *registers, const SMD_RE
 			snprintf(client_write_string, sizeof(client_write_string), "%s%s", client_write_string, "\n");
 			
 			//write the registers to the client
-			write_to_client(cl, client_write_string);
+			write_to_client(cmd_args->cl, client_write_string);
 		}
 		
 		modbus_close(ctx);
 		modbus_free(ctx);
 		
-		return SMD_RETURN_COMMAND_SUCCESS;
+		cmd_args->result = SMD_RETURN_COMMAND_SUCCESS;
 	}
-
+	
+	pthread_exit(NULL);
+	return 0;
 }
+
