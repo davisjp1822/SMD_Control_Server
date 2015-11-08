17c43c7d0ccdc27e69f6ec733c1a81b3857c8a66
John Davis <jd@linecardapp.com>
2015-11-08 13:38:02 -0500

    fixing build script

--- SMD_Control_Server/build.sh
+++ SMD_Control_Server/build.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#/bin/bash
 
-export jm_cv_func_working_malloc="yes"
-export ac_cv_func_malloc_0_nonnull="yes"
-export ac_cv_func_realloc_works="yes"
+if [ -f "/etc/arduino/openwrt-yun-release" ]; then
+	
+	echo "We are building on the Yun!"
+	
+	#environment variables that fix the uClibc issues
+	export jm_cv_func_working_malloc="yes"
+	export ac_cv_func_malloc_0_nonnull="yes"
+	export ac_cv_func_realloc_works="yes"
 
-./configure CPPFLAGS="-I/mnt/sda1/smd_server/include/modbus" LDFLAGS="-L/mnt/sda1/smd_server/lib"
+	./configure CPPFLAGS="-I/mnt/sda1/smd_server/include/modbus" LDFLAGS="-L/mnt/sda1/smd_server/lib"
+
+fi
+
